b0e56903584e7f72032c89088fc06f3dff8c62f2
Jens-Uwe Mager <jum@anubis.han.de>
2013-05-13 19:30:57 -0500

    Add a second server, this time a very slow one and allow the test directory listing to be run with parameters.

--- tinyftp_test.go
+++ tinyftp_test.go
@@ -2,15 +2,18 @@ package tinyftp
 
 import (
 	"net"
+	"time"
 	"testing"
 )
 
 const (
-	ftpHost = "prep.ai.mit.edu:21"
-	ftpDir  = "/gnu/chess"
+	ftpHost1 = "prep.ai.mit.edu:21"
+	ftpDir1  = "/gnu/chess"
+	ftpHost2 = "ftpprd.ncep.noaa.gov:21"
+	ftpDir2  = "/pub/data/nccf/com/rtofs/prod/rtofs."
 )
 
-func TestTinyFTP(t *testing.T) {
+func testNameList(t *testing.T, ftpHost, ftpDir string) {
 	c, code, msg, err := Dial("tcp", ftpHost)
 	if err != nil {
 		t.Fatal(err)
@@ -66,4 +69,12 @@ func TestTinyFTP(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Logf("code %d, msg %v", code, msg)
+}
+
+func TestTinyFTP1(t *testing.T) {
+	testNameList(t, ftpHost1, ftpDir1)
+}
+
+func TestTinyFTP2(t *testing.T) {
+	testNameList(t, ftpHost2, ftpDir2 + time.Now().Format("20060102"))
 }
